22b3842aabeea536482008f84968a5d25c5435d1
Heky <syahekytif@gmail.com>
2024-10-28 01:43:44 +0700

    api dashboard

--- routes/api.php
+++ routes/api.php
@@ -12,40 +12,44 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LoginController;
 use App\Http\Controllers\API\LogoutController;
+use App\Models\User; // Pastikan untuk mengimpor model User
 
+// Mengambil data pengguna yang sedang login
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return response()->json($request->user()); // Mengembalikan data pengguna yang sedang login
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+// Rute untuk registrasi
 Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::get('/user', [UserController::class, 'index']);
-// Route::post("/logout", [LogoutController::class, 'logout']);
-Route::middleware('auth:sanctum')->post('/logout', [LogoutController::class , 'logout']);
 
-//Dosen
+// Rute untuk login
+Route::post('/login', [LoginController::class, 'login']);
+
+// Rute untuk logout
+Route::middleware('auth:sanctum')->post('/logout', [LogoutController::class, 'logout']);
+
+// Dosen
 Route::get('/dosen', [DosenController::class, 'dosen']);
 Route::post('/create-dosen', [DosenController::class, 'CreateDosen']);
 Route::put('/update-dosen/{id_dosen}', [DosenController::class, 'UpdateDosen']);
 Route::delete('/delete-dosen/{id_dosen}', [DosenController::class, 'DeleteDosen']);
 Route::get('/melihat-dosen/{id_dosen}', [DosenController::class, 'MelihatDosen']);
 
-//Jadwal
+// Jadwal
 Route::get('/jadwal', [JadwalController::class, 'Jadwal']);
 Route::post('/create-jadwal', [JadwalController::class, 'CreateJadwal']);
 Route::put('/update-jadwal/{id_jadwal}', [JadwalController::class, 'UpdateJadwal']);
-Route::delete('delete-jadwal/{id_jadwal}', [JadwalController::class, 'DeleteJadwal']);
+Route::delete('/delete-jadwal/{id_jadwal}', [JadwalController::class, 'DeleteJadwal']);
 Route::get('/melihat-jadwal/{id}', [JadwalController::class, 'MelihatJadwal']);
 
-//fasilitas
+// Fasilitas
 Route::get('/fasilitas', [FasilitasController::class, 'Fasilitas']);
 Route::post('/create-fasilitas', [FasilitasController::class, 'CreateFasilitas']);
 Route::get('/melihat-fasilitas/{id_fasilitas}', [FasilitasController::class, 'MelihatFasilitas']);
 Route::put('/update-fasilitas/{id_fasilitas}', [FasilitasController::class, 'UpdateFasilitas']);
 Route::delete('/delete-fasilitas/{id_fasilitas}', [FasilitasController::class, 'DeleteFasilitas']);
 
-//kelas
+// Kelas
 Route::get('/kelas', [KelasController::class, 'Kelas']);
 Route::post('/create-kelas', [KelasController::class, 'CreateKelas']);
 Route::put('/update-kelas/{id_kelas}', [KelasController::class, 'UpdateKelas']);
@@ -53,7 +57,7 @@ Route::delete('/delete-kelas/{id_kelas}', [KelasController::class, 'DeleteKelas'
 Route::post('/borrow-kelas/{id_kelas}', [KelasController::class, 'PinjamKelas']);
 Route::get('/image/{filename}', [KelasController::class, 'getImage']);
 
-//mata_kuliah
+// Mata Kuliah
 Route::get('/mata-kuliah', [MataKuliahController::class, 'MataKuliah']);
 Route::post('/create-mata-kuliah', [MataKuliahController::class, 'CreateMataKuliah']);
 Route::put('/update-mata-kuliah/{id_matkul}', [MataKuliahController::class, 'UpdateMataKuliah']);
